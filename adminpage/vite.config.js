e2ffe64940f2c2f50b9f629e0f4cbc0eea168599
Pruthviraj3196 <116255942+Pruthviraj3196@users.noreply.github.com>
2024-06-22 01:26:49 +0530

    Update vite.config.js

--- adminpage/vite.config.js
+++ adminpage/vite.config.js
@@ -3,5 +3,16 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  server:{
+    host: '0.0.0.0',
+    port: 10000,
+    proxy:{
+      '/api':{
+        target:'https://zomato-clone-n3s4.onrender.com',
+        changeOrigin:true,
+        secure:false
+      },
+    },
+  },
   plugins: [react()],
 })
